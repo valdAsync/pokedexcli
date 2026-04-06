bd7bf6b5ea31b89bca73fab94e7bc7ac282f57f8
valdAsync <valdAsync@proton.me>
2026-04-06 15:10:22 +0300

    main loop

--- repl.go
+++ repl.go
@@ -1,6 +1,28 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	reader := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		reader.Scan()
+
+		words := cleanInput(reader.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+
+		fmt.Printf("Your command was: %s\n", commandName)
+	}
+}
 
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
